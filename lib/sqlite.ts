a95d4951cd135b199e92eb7c59c7aadc49a768a1
ztoufik <toufikouedtlelat@gmail.com>
2025-06-21 18:53:32 +0200

    using schema approach to clean up types interface

--- lib/sqlite.ts
+++ lib/sqlite.ts
@@ -1,56 +1,83 @@
 import {Myblob,Commit,Ref} from './types';
 import type { IDataLayer } from './data';
-import { DataSource} from "typeorm"
-
+import { EntitySchema,DataSource} from "typeorm"
 
 
 /*class SqliteHasher implements IDataLayer{
-    orm:MikroORM;
-    constructor(name:string){
-        this.orm = MikroORM.initSync<SqliteDriver>({
-            entities: [Myblob,Commit,Ref],
-            dbName: name,
-            type:'sqlite',
-        });
+  orm:MikroORM;
+  constructor(name:string){
+  this.orm = MikroORM.initSync<SqliteDriver>({
+  entities: [Myblob,Commit,Ref],
+  dbName: name,
+  type:'sqlite',
+  });
 
-    }
+  }
 
-    create_blob(blob:Myblob):void{
-        this.orm.em.persistAndFlush(blob)
-    }
+  create_blob(blob:Myblob):void{
+  this.orm.em.persistAndFlush(blob)
+  }
 
-    async retrieve_blob(hash_id:string):Promise<Myblob>{
-        return this.orm.em.findOne(Myblob, {hash_id}).then(_blob=> _blob);
-    }
+  async retrieve_blob(hash_id:string):Promise<Myblob>{
+  return this.orm.em.findOne(Myblob, {hash_id}).then(_blob=> _blob);
+  }
 
-    create_commit(commit:Commit):void{
-        this.orm.em.persistAndFlush(commit);
-    }
-    async retrieve_commit(hash_id:string):Promise<Commit>|null{
-        return this.orm.em.findOne(Commit, {hash_id}).then(commit=> commit);
-    }
+  create_commit(commit:Commit):void{
+  this.orm.em.persistAndFlush(commit);
+  }
+  async retrieve_commit(hash_id:string):Promise<Commit>|null{
+  return this.orm.em.findOne(Commit, {hash_id}).then(commit=> commit);
+  }
 
-    create_ref(ref:Ref):void{
-        this.orm.em.persistAndFlush(ref);
-    }
+  create_ref(ref:Ref):void{
+  this.orm.em.persistAndFlush(ref);
+  }
 
-    update_ref(old_ref:Ref,new_ref:Ref):void{
-        const old_obj=this.orm.em.findOne(Ref, {name:old_ref.name}).then(commit=> commit);
-    }
+  update_ref(old_ref:Ref,new_ref:Ref):void{
+  const old_obj=this.orm.em.findOne(Ref, {name:old_ref.name}).then(commit=> commit);
+  }
 
-    retrieve_ref(name:string):Ref{
-        return this.orm.em.findOne(Ref, {name}).then(ref=> ref);
-    }
-}*/
+  retrieve_ref(name:string):Ref{
+  return this.orm.em.findOne(Ref, {name}).then(ref=> ref);
+  }
+  }*/
+
+const BlobSchema = new EntitySchema<Myblob>({
+    name: 'Blob',
+    tableName: 'blobs', // Optional: specify table name if different from entity name
+    columns: {
+        hash_id: {
+            type: String,
+            primary: true,
+        },
+        path:{
+            type: String,
+            nullable:false,
+        },
+        content:{
+            type: String,
+            nullable:false,
+        },
+        blob_type:{
+            type: String,
+            nullable:false,
+        },
+        date:{
+            type: String,
+            nullable:false,
+        },
+    },
+})
 
 const AppDataSource = new DataSource({
     type: "sqlite",
     database: "tgit.sql",
-    entities: [Myblob],
-    logging: false,
+    entities: [BlobSchema],
+    logging: true,
     synchronize: true,
 })
 
 await AppDataSource.initialize()
 
-export const repo =AppDataSource.getRepository(Myblob);
+export const repo =AppDataSource.getRepository(BlobSchema);
+
--- lib/types.ts
+++ lib/types.ts
@@ -1,46 +1,19 @@
-import { Entity, PrimaryColumn, Column} from "typeorm"
-
-
-@Entity({name:"myblob"})
 export class Myblob {
-    @PrimaryColumn("text")
     hash_id!:string;
 
-    @Column()
     path!:string;
 
-    @Column()
     blob_type!:string
 
-    @Column("date")
     date!:Date;
 
-    @Column("text")
     content!:string;
 };
 
-@Entity()
 export class Commit {
-    @PrimaryColumn()
-    hash_id!:string;
-    @Column("text")
-    content!:string;
-    @Column()
-    parent!:Commit[];
-    @Column("date")
-    date!:Date;
 };
 
-@Entity()
 export class Ref {
-    @PrimaryColumn()
-    name!:string;
-    @Column()
-    ref_type!:string;
-    @Column("text")
-    content!:string;
-    @Column("date")
-    date!:Date;
 }
 
 export class File_Error{
